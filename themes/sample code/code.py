eea4a35dd7eda05603bb35b990f6c6d31aba57bb
lioshi <lionel.fenneteau@gmail.com>
2017-01-24 10:33:06 +0100

    new code samples

--- themes/sample code/code.py
+++ themes/sample code/code.py
@@ -25,3 +25,29 @@ class TestMedian(unittest.TestCase):
 print queens
 print "\n".join(". "*q + "Q " + ". "*(BOARD_SIZE-q-1) for q in queens)
 
+
+
+import random
+n = random.randint(1, 99)
+guess = int(raw_input("Enter an integer from 1 to 99: "))
+while n != "guess":
+    print
+    if guess < n:
+        print "guess is low"
+        guess = int(raw_input("Enter an integer from 1 to 99: "))
+    elif guess > n:
+        print "guess is high"
+        guess = int(raw_input("Enter an integer from 1 to 99: "))
+    else:
+        print "you guessed it!"
+        break
+    print
+
+
+
+#!/usr/bin/env python
+Celsius = int(raw_input("Enter a temperature in Celsius: "))
+
+Fahrenheit = 9.0/5.0 * Celsius + 32
+
+print "Temperature:", Celsius, "Celsius = ", Fahrenheit, " F"    
