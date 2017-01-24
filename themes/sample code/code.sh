292ff621bea8753dfec297cb6c2c9865b847a2fb
lioshi <lionel.fenneteau@gmail.com>
2017-01-24 11:00:10 +0100

    some samples adding

--- themes/sample code/code.sh
+++ themes/sample code/code.sh
@@ -4,3 +4,32 @@
 echo "My first name is $1"
 echo "My surname is $2"
 echo "Total number of arguments is $#" 
+
+# use predefined variables to access passed arguments
+#echo arguments to the shell
+echo $1 $2 $3 ' -> echo $1 $2 $3'
+
+# We can also store arguments from bash command line in special array
+args=("$@")
+#echo arguments to the shell
+echo ${args[0]} ${args[1]} ${args[2]} ' -> args=("$@"); echo ${args[0]} ${args[1]} ${args[2]}'
+
+#use $@ to print out all arguments at once
+echo $@ ' -> echo $@'
+
+# use $# variable to print out
+# number of arguments passed to the bash script
+echo Number of arguments passed: $# ' -> echo Number of arguments passed: $#' 
+
+# bash prints message => Executing bash trap subrutine !
+bashtrap()
+{
+    echo "CTRL+C Detected !...executing bash trap !"
+}
+# for loop from 1/10 to 10/10
+for a in `seq 1 10`; do
+    echo "$a/10 to Exit." 
+    sleep 1;
+done
+echo "Exit Bash Trap Example!!!"
+
