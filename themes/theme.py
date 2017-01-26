be01d74b9417f8c3db2c900cf198369521ddb479
lioshi <lionel.fenneteau@gmail.com>
2017-01-26 18:03:21 +0100

    add colors

--- themes/theme.py
+++ themes/theme.py
@@ -32,6 +32,52 @@ for dictA in TREE.xpath("dict/array/dict/key"):
                 print colored.cyan("background")+" "+background.text
         print "\n"
 
+# mettre tout ca dans un tableau pour traitement des couleurs
+
+#2d2d2d Background
+#393939 Current Line
+#515151 Selection
+#c5c8c6 Foreground
+#666666 Comment
+#cc6666 Red
+#de935f Orange
+#f0c674 Yellow
+#b5bd68 Green
+#5d968d Aqua
+#81a2be Blue
+#be94bb Purple
+
+
+
+
+
+#CC6666
+#CC7466
+#CC8166
+
+#DE935F
+#DEA15F
+#DEAF5F
+
+#F0C674
+#F0D274
+#F0DE74
+
+#B5BD68
+#92B960
+#69B24F
+
+#5D968D
+#5D9196
+#5D8599
+
+#81A2BE
+#818CBE
+#8381BE
+
+#BE94BB
+#BE94A1
+#BE9D94
 
 
 
